f699e24ba09f55a88271579b3978da5522e5a009
Dinushi Ariyasena <dinushi.ariyasena@outlook.com>
2022-01-17 08:38:14 +0530

    :sparkles:feat: create hero section

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -1,2 +1,22 @@
-const HeroSection: React.FC = () => <h1>HeroSection</h1>;
+import { Container, Row } from "react-bootstrap";
+
+const HeroSection: React.FC = () => {
+  return (
+    <Container fluid className="hero">
+      <Row className="hero__bg"></Row>
+      <Row className="hero__credits">
+        <p className="text-end">
+          Photo by{" "}
+          <a href="https://unsplash.com/@annahunko?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Anna Hunko
+          </a>{" "}
+          on{" "}
+          <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </p>
+      </Row>
+    </Container>
+  );
+};
 export default HeroSection;
