e27f0f8c745bdd6a2beb50b1d6774ee126be995d
kesaralive@gmail.com <90370604+kesaralive@users.noreply.github.com>
2022-01-18 16:44:46 +0530

    :art:style: improve the layout structure

--- src/sections/DetailSection.tsx
+++ src/sections/DetailSection.tsx
@@ -1,10 +1,17 @@
+import { Container, Row, Col } from "react-bootstrap";
 import AuthorList from "../components/AuthorList";
 import BookList from "../components/BookList";
 
 const DetailSection: React.FC = () => (
-  <div>
-    <AuthorList />
-    <BookList />
-  </div>
+  <Container fluid className="detail px-5 ">
+    <Row className="flex-column flex-lg-row justify-content-between gap-5">
+      <Col>
+        <BookList />
+      </Col>
+      <Col>
+        <AuthorList />
+      </Col>
+    </Row>
+  </Container>
 );
 export default DetailSection;
