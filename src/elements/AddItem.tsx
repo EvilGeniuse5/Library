7142a01627d9d19f3983e5fd37bce0281d0363db
Nawoda Jayasinghe <nawoda.jayasinghe98@gmail.com>
2022-01-22 19:39:32 +0530

    :sparkles:feat: create additem component

--- src/elements/AddItem.tsx
+++ src/elements/AddItem.tsx
@@ -1,2 +1,8 @@
-const AddItem: React.FC = () => <div>AddItem</div>;
+import {Plus} from "react-feather";
+const AddItem: React.FC = () =>{
+    return(
+    <div className="add-item">
+    <Plus className="add-item__plus"/>Add Item</div>
+    )
+};
 export default AddItem;
