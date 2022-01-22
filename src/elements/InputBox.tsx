4d6ce8bd7dedebf40c228e34bf6b4b9b9d8a7e0d
kesaralive@gmail.com <90370604+kesaralive@users.noreply.github.com>
2022-01-22 21:49:24 +0530

    :sparkles:feat: create inputbox component

--- src/elements/InputBox.tsx
+++ src/elements/InputBox.tsx
@@ -1,2 +1,9 @@
-const InputBox: React.FC = () => <h1>InputBox</h1>;
+import { Form } from "react-bootstrap";
+const InputBox: React.FC = () => (
+  <div className="input-box">
+    {" "}
+    <p className="input-box__title">Title of the Book</p>
+    <Form.Control className="input-box__input" type="email" />
+  </div>
+);
 export default InputBox;
