794fafb6fedce04418372b2b06290a3e033a2c51
kesaralive@gmail.com <90370604+kesaralive@users.noreply.github.com>
2022-02-12 22:39:18 +0530

    :lipstick:fix: rename style files in kebab case

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Navbar from './sections/Navbar';
 import HeroSection from './sections/HeroSection';
 import DetailSection from './sections/DetailSection';
+import WelcomeTitle from './sections/WelcomeTitle';
 
 function App() {
 	return (
 		<div className='App pb-5'>
-			<Navbar />
+			<WelcomeTitle />
 			<HeroSection />
 			<DetailSection />
 			{/* <img src="/img/empty.jpg" alt="img" className="pixel-perfect" /> */}
