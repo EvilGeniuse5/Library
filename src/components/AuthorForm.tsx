8f4b4d512681f8e4a19626a7c763f8c484a0f3a0
kesaralive@gmail.com <90370604+kesaralive@users.noreply.github.com>
2022-02-14 18:11:51 +0530

    :lipstick:style: update ux & use bootstrap classes

--- src/components/AuthorForm.tsx
+++ src/components/AuthorForm.tsx
@@ -6,11 +6,12 @@ import { Button, Col, Form, Row } from 'react-bootstrap';
 const AuthorForm: React.FC<{
 	className: string;
 	onclick: () => void;
+	authorList: Array<string>;
 	createAuthor: (author: string) => void;
-}> = ({ className, onclick, createAuthor }) => {
+}> = ({ className, onclick, createAuthor, authorList }) => {
 	const initialValues = { authorName: '' };
 	const [authors, setAuthors] = useState(initialValues);
-	const [authorErrors, setAuthorErrors] = useState({});
+	const [authorErrors, setAuthorErrors] = useState({ authorName: '' });
 	const [isSubmit, setIsSubmit] = useState(false);
 
 	const handleChange = (e: any) => {
@@ -25,9 +26,9 @@ const AuthorForm: React.FC<{
 	};
 
 	useEffect(() => {
-		console.log(authorErrors);
 		if (Object.keys(authorErrors).length === 0 && isSubmit) {
-			console.log(authors);
+			createAuthor(authors.authorName);
+			setAuthors(initialValues);
 		}
 	}, [authorErrors]);
 
@@ -35,41 +36,58 @@ const AuthorForm: React.FC<{
 		const errors: any = {};
 		if (!values.authorName) {
 			errors.authorName = 'Author name is required';
-		} else if (values.authorName.match(validName) === true) {
+		} else if (!validName.test(values.authorName)) {
 			errors.authorName = 'Author name is invalid';
+		} else if (authorList.includes(values.authorName)) {
+			errors.authorName = 'Author name already exists';
 		}
 		return errors;
 	};
 
 	return (
 		<Row className={'library-form ' + className}>
-			<Col xs={12} xxl={8} className='library-form__form'>
-				<div className='d-flex align-items-center justify-content-between mb-4'>
-					<div className='library-form__title'>
-						<h1>Create Author</h1>
-						<hr />
-					</div>
-					<span onClick={onclick}>
-						<XCircle className='library-form__xcircle' />
-					</span>
-				</div>
+			<Col xs={12} xxl={9} className='library-form__form ps-lg-0 pe-lg-5 '>
 				<Row>
-					<pre>{JSON.stringify(authors, undefined, 2)}</pre>
+					<div className='d-flex align-items-center justify-content-between my-4 px-0'>
+						<div className='library-form__title'>
+							<h1>Create Author</h1>
+							<hr />
+						</div>
+						<span onClick={onclick}>
+							<XCircle className='library-form__xcircle' />
+						</span>
+					</div>
+				</Row>
 
-					<Form onSubmit={handleSubmit}>
-						<Form.Group>
-							<Form.Label>Email address</Form.Label>
+				<Row>
+					<Form onSubmit={handleSubmit} className='p-0 my-0'>
+						<Form.Group className='ms-lg-5 my-2'>
+							<Form.Label className='mb-1 library-form__input-title'>
+								Author name
+							</Form.Label>
 							<Form.Control
 								type='text'
 								name='authorName'
 								value={authors.authorName}
 								onChange={handleChange}
+								style={{
+									borderColor: authorErrors.authorName ? '#dc3545' : '',
+								}}
 							/>
+							<Form.Control.Feedback
+								type='invalid'
+								style={{
+									display: authorErrors.authorName ? 'block' : 'none',
+									fontWeight: 500,
+								}}
+							>
+								{authorErrors.authorName}
+							</Form.Control.Feedback>
 						</Form.Group>
 						<Col className='create-button d-flex justify-content-end'>
 							<Button
 								type='submit'
-								className='create-button__button btn btn-primary'
+								className='create-button__button btn btn-primary mt-4'
 							>
 								Create
 							</Button>
